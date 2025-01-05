36e9daaf35348df0fafac2a1bc6313b35e5a9572
Roncelli <leonardoroncelli@gmail.com>
2025-01-05 16:03:48 +0100

    Classe DipendenteStipendiato

--- src/DipendenteStipendiato.java
+++ src/DipendenteStipendiato.java
@@ -1,2 +1,18 @@
-public class DipendenteStipendiato {
+public class DipendenteStipendiato extends Dipendente {
+    private double stipendioSettimanale;
+    public DipendenteStipendiato(String nome, String cognome, String codiceFiscale, double stipendioSettimanale) {
+        super(nome, cognome, codiceFiscale);
+        this.stipendioSettimanale = stipendioSettimanale;
+    }
+    public double getStipendioSettimanale() {
+        return stipendioSettimanale;
+    }
+    public void setStipendioSettimanale(double stipendioSettimanale) {
+        this.stipendioSettimanale = stipendioSettimanale;
+    }public double paga() {
+        return stipendioSettimanale;
+    }
+    public String toString() {
+        return "Dipendente Stipendiato: " + getNome() + " " + getCognome() + ", Stipendio: " + stipendioSettimanale;
+    }
 }
