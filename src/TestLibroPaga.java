21cee0cd8891a16afd012caa153816b1746e6818
Roncelli <leonardoroncelli@gmail.com>
2025-01-05 16:07:30 +0100

    Classe TestLibroPaga

--- src/TestLibroPaga.java
+++ src/TestLibroPaga.java
@@ -1,2 +1,16 @@
 public class TestLibroPaga {
+    public static void main(String[] args) {
+        LibroPaga libroPaga = new LibroPaga();
+        libroPaga.aggiungiDipendente(new DipendenteStipendiato("Mario", "Rossi", "RSSMRA80A01H501Z", 1000));
+        libroPaga.aggiungiDipendente(new DipendentePagatoAOre("Giovanni", "Bianchi", "BNCGNN85M01Z404V", 20, 45));
+        libroPaga.aggiungiDipendente(new DipendenteSuCommissione("Anna", "Verdi", "VRDANN78S45J320R", 0.05, 5000));
+        libroPaga.aggiungiDipendente(new DipendenteCommissionePlusBase("Lucia", "Neri", "NRLLCU72V60C351Z", 800, 0.03, 2000));
+        System.out.println("Dipendenti:");
+        libroPaga.stampaDipendenti();
+        System.out.println("\nDipendenti Ordinati per Stipendio:");
+        libroPaga.stampaDipendentiOrdinati();
+        libroPaga.modificaStipendioBase();
+        System.out.println("\nDipendenti dopo modifica stipendio base:");
+        libroPaga.stampaDipendenti();
+    }
 }
