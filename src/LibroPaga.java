a1d9455960f703a290d74a0c2d07ba3b70faeabd
Roncelli <leonardoroncelli@gmail.com>
2025-01-05 16:07:01 +0100

    Classe LibroPaga

--- src/LibroPaga.java
+++ src/LibroPaga.java
@@ -1,2 +1,26 @@
+import java.util.*;
 public class LibroPaga {
+    private List<Dipendente> dipendenti;
+    public LibroPaga() {
+        dipendenti = new ArrayList<>();
+    }public void aggiungiDipendente(Dipendente dipendente) {
+        dipendenti.add(dipendente);
+    }
+    public void stampaDipendenti() {
+        for (Dipendente d : dipendenti) {
+            System.out.println(d);
+        }
+    }
+    public void stampaDipendentiOrdinati() {
+        Collections.sort(dipendenti);
+        stampaDipendenti();
+    }
+    public void modificaStipendioBase() {
+        for (Dipendente d : dipendenti) {
+            if (d instanceof DipendenteCommissionePlusBase) {
+                DipendenteCommissionePlusBase dcpb = (DipendenteCommissionePlusBase) d;
+                dcpb.setStipendioBase(dcpb.getStipendioBase() * 1.1); // Aggiungi il 10%
+            }
+        }
+    }
 }
